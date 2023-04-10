c65f9fd5d7a1b6bcbf8d8ba28d8b84542345ba50
bao.tran5 <bao.tran5@gmail.com>
2023-04-10 16:37:08 +0700

    Initial commit

---
+++ src/main/java/com/baotg/raven/RavenApplication.java
@@ -0,0 +1,13 @@
+package com.baotg.raven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RavenApplication.class, args);
+    }
+
+}
